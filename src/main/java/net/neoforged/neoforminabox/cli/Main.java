d8722a6dbafa9a84032bfd100d700b37d3189888
Sebastian Hartte <shartte@users.noreply.github.com>
2024-05-05 15:08:55 +0200

    Fix parameter scope

--- src/main/java/net/neoforged/neoforminabox/cli/Main.java
+++ src/main/java/net/neoforged/neoforminabox/cli/Main.java
@@ -12,16 +12,16 @@ import static picocli.CommandLine.Option;
 
 @Command(subcommands = {RunNeoFormCommand.class, DownloadAssetsCommand.class}, mixinStandardHelpOptions = true)
 public class Main {
-    @Option(names = "--home-dir")
+    @Option(names = "--home-dir", scope = CommandLine.ScopeType.INHERIT)
     Path cacheDir = Paths.get(System.getProperty("user.home")).resolve(".neoform");
 
-    @Option(names = "--repository", arity = "*")
+    @Option(names = "--repository", arity = "*", scope = CommandLine.ScopeType.INHERIT)
     List<URI> repositories = List.of(URI.create("https://maven.neoforged.net/releases/"));
 
-    @Option(names = "--artifact-manifest")
+    @Option(names = "--artifact-manifest", scope = CommandLine.ScopeType.INHERIT)
     Path artifactManifest;
 
-    @CommandLine.Option(names = "--launcher-meta-uri")
+    @CommandLine.Option(names = "--launcher-meta-uri", scope = CommandLine.ScopeType.INHERIT)
     URI launcherManifestUrl = URI.create("https://launchermeta.mojang.com/mc/game/version_manifest_v2.json");
 
     public static void main(String... args) {
