9341c2c9760ddd213ede60cc9343b8e50cb445a0
Sebastian Hartte <shartte@users.noreply.github.com>
2024-06-23 18:35:09 +0200

    Legacy

--- src/main/java/net/neoforged/neoform/runtime/config/neoforge/NeoForgeConfig.java
+++ src/main/java/net/neoforged/neoform/runtime/config/neoforge/NeoForgeConfig.java
@@ -3,11 +3,13 @@ package net.neoforged.neoform.runtime.config.neoforge;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
+import net.neoforged.neoform.runtime.utils.FilenameUtil;
 import net.neoforged.neoform.runtime.utils.MavenCoordinate;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipFile;
@@ -39,6 +41,31 @@ public record NeoForgeConfig(
                 .create();
         var root = gson.fromJson(new StringReader(new String(configContent, StandardCharsets.UTF_8)), JsonObject.class);
 
+        var specVersion = root.getAsJsonPrimitive("spec").getAsInt();
+        if (specVersion != 2) {
+            throw new IOException("Unsupported NeoForge spec version: " + specVersion);
+        }
+
+        // Forge and NeoForge used the same format, but Forge used list of paths for "ats"
+        if (root.get("ats").isJsonArray()) {
+            convertAccessTransformerPropertyFromForgeToNeoForge(root);
+        }
+
         return gson.fromJson(root, NeoForgeConfig.class);
+    }
+
+    private static void convertAccessTransformerPropertyFromForgeToNeoForge(JsonObject root) {
+        var ats = root.get("ats").getAsJsonArray();
+        var atFiles = new ArrayList<String>();
+        for (var at : ats) {
+            atFiles.add(at.getAsString());
+        }
+
+        var longestPrefix = FilenameUtil.longestCommonDirPrefix(atFiles);
+        if (longestPrefix == null) {
+            root.remove("ats");
+        } else {
+            root.addProperty("ats", longestPrefix);
+        }
     }
 }
