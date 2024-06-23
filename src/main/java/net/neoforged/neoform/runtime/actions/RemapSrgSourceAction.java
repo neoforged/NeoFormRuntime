d4408267c0ebd9c2dbc61327e6395c0c56a7f7cd
Sebastian Hartte <shartte@users.noreply.github.com>
2024-06-23 18:35:09 +0200

    Legacy

---
+++ src/main/java/net/neoforged/neoform/runtime/actions/RemapSrgSourceAction.java
@@ -0,0 +1,12 @@
+package net.neoforged.neoform.runtime.actions;
+
+import net.neoforged.neoform.runtime.engine.ProcessingEnvironment;
+
+import java.io.IOException;
+
+public class RemapSrgSourceAction extends BuiltInAction {
+    @Override
+    public void run(ProcessingEnvironment environment) throws IOException, InterruptedException {
+
+    }
+}
