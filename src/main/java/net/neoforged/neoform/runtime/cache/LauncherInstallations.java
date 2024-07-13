0b58aa4d01812741236b1e4ec4ba2bf8459e1f7b
Sebastian Hartte <shartte@users.noreply.github.com>
2024-07-13 19:43:33 +0200

    Added FTB App Paths for Linux and Mac OS

--- src/main/java/net/neoforged/neoform/runtime/cache/LauncherInstallations.java
+++ src/main/java/net/neoforged/neoform/runtime/cache/LauncherInstallations.java
@@ -30,8 +30,7 @@ public class LauncherInstallations {
             "${env.APPDATA}/com.modrinth.theseus/meta/", // Windows, Modrinth App
             "${env.LOCALAPPDATA}/.ftba/bin/", // Windows, FTB App
             "${user.home}/.ftba/bin/", // linux, FTB App
-            "${user.home}/Library/Application Support/.ftba/bin/", // linux, FTB App
-            "${env.LOCALAPPDATA}/.ftba/bin/", // macOS, FTB App
+            "${user.home}/Library/Application Support/.ftba/bin/", // macos, FTB App
             "${user.home}/.local/share/PrismLauncher/", // linux, PrismLauncher
             "${user.home}/.local/share/multimc/", // linux, MultiMC
             "${user.home}/Library/Application Support/PrismLauncher/", // macOS, PrismLauncher
