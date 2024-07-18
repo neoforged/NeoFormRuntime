6f59f52f4d0fa8ccb7a231f97311210a80a20239
Sebastian Hartte <shartte@users.noreply.github.com>
2024-07-19 00:27:40 +0200

    The attribute view implicitly checks for existence.

--- src/main/java/net/neoforged/neoform/runtime/artifacts/Artifact.java
+++ src/main/java/net/neoforged/neoform/runtime/artifacts/Artifact.java
@@ -2,16 +2,12 @@ package net.neoforged.neoform.runtime.artifacts;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public record Artifact(Path path, long lastModified, long size) {
     public static Artifact ofPath(Path path) throws IOException {
-        if (!Files.isRegularFile(path)) {
-            throw new NoSuchFileException(path.toString());
-        }
-        var attrView = Files.getFileAttributeView(path, BasicFileAttributeView.class).readAttributes();
-        return new Artifact(path, attrView.lastModifiedTime().toMillis(), attrView.size());
+        var attributes = Files.readAttributes(path, BasicFileAttributes.class);
+        return new Artifact(path, attributes.lastModifiedTime().toMillis(), attributes.size());
     }
 }
