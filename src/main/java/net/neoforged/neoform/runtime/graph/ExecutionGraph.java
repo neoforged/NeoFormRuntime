15fb460a6e8d594a88443ce95f3a9e81dd89b65b
Luke Bemish <lukebemish@lukebemish.dev>
2024-08-12 17:44:57 -0500

    Chane to debug format for individual steps

--- src/main/java/net/neoforged/neoform/runtime/graph/ExecutionGraph.java
+++ src/main/java/net/neoforged/neoform/runtime/graph/ExecutionGraph.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class ExecutionGraph {
     private final Map<String, ExecutionNode> nodes = new LinkedHashMap<>();
@@ -42,7 +43,17 @@ public class ExecutionGraph {
     }
 
     public NodeOutput getResult(String id) {
-        return results.get(id);
+        var output = results.get(id);
+        if (output != null) {
+            return output;
+        }
+        var matcher = Pattern.compile("^node\\.([a-zA-Z0-9]+)\\.output\\.([a-zA-Z0-9]+)$").matcher(id);
+        if (matcher.matches()) {
+            var step = matcher.group(1);
+            var outputId = matcher.group(2);
+            return getRequiredOutput(step, outputId);
+        }
+        return null;
     }
 
     public Map<String, NodeOutput> getResults() {
