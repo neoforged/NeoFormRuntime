07197e7489a653a3d814c67cd7979d03dacd4a85
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-09-04 21:57:44 +0200

    Fix asset index caching (#43)
    We need to override `checksum()` and `checksumAlgorithm()` for the
    downloader to reuse a local file.

--- src/main/java/net/neoforged/neoform/runtime/manifests/AssetIndexReference.java
+++ src/main/java/net/neoforged/neoform/runtime/manifests/AssetIndexReference.java
@@ -5,6 +5,10 @@ import net.neoforged.neoform.runtime.downloads.DownloadSpec;
 
 import java.net.URI;
 
-public record AssetIndexReference(String id, String sha1, int size, long totalSize,
+public record AssetIndexReference(String id, @SerializedName("sha1") String checksum, int size, long totalSize,
                                   @SerializedName("url") URI uri) implements DownloadSpec {
+    @Override
+    public String checksumAlgorithm() {
+        return "sha1";
+    }
 }
